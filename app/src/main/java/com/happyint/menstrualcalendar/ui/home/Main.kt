2fd9ed61c80506c333be902af85ff7b1a97d8c01
SewonYun <facemen9459@gmail.com>
2023-07-01 13:26:38 +0900

    [fix] Depart of component from Main.kt file

--- app/src/main/java/com/happyint/menstrualcalendar/ui/home/Main.kt
+++ app/src/main/java/com/happyint/menstrualcalendar/ui/home/Main.kt
@@ -1,33 +1,18 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
-
 package com.happyint.menstrualcalendar.ui.home
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.paint
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.happyint.menstrualcalendar.R
+import com.happyint.menstrualcalendar.ui.home.middle.MiddleContent
+import com.happyint.menstrualcalendar.ui.home.middle.MiddleTab
+import com.happyint.menstrualcalendar.ui.home.top.TopBar
+import com.happyint.menstrualcalendar.ui.home.top.TopSwipeContent
 
 @Composable
 @Preview
@@ -42,66 +27,10 @@ fun LoadMainHome() {
 
         Spacer(modifier = Modifier.height(32.dp))
 
+        val state = remember { mutableStateOf(0) }
+        MiddleTab(state)
+        Spacer(modifier = Modifier.height(10.dp))
+        MiddleContent(state)
     }
 
-
-}
-
-@Composable
-fun TopSwipeContent(cb: () -> Unit) {
-    Card(
-        onClick = cb,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(10.dp)
-            .height(300.dp)
-    ) {
-        Box(
-            Modifier
-                .paint(
-                    painterResource(id = R.drawable.card_sample1),
-                    contentScale = ContentScale.Crop,
-
-                    )
-                .fillMaxSize()
-        ) {
-            Text("Clickable", Modifier.align(Alignment.Center))
-        }
-    }
-}
-
-//@Preview
-//@Composable
-//fun TopSwipeContentPreview() {
-//    TopSwipeContent {}
-//}
-
-//@Preview
-@Composable
-fun TopBar() {
-    TopAppBar(
-        title = {
-            Text(
-                "Large TopAppBar",
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-        },
-        navigationIcon = {
-            IconButton(onClick = { /* doSomething() */ }) {
-                Icon(
-                    imageVector = Icons.Filled.Menu,
-                    contentDescription = "Localized description"
-                )
-            }
-        },
-        actions = {
-            IconButton(onClick = { /* doSomething() */ }) {
-                Icon(
-                    imageVector = Icons.Filled.Favorite,
-                    contentDescription = "Localized description"
-                )
-            }
-        }
-    )
 }
