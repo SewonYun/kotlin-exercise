604a20eb3ff6130d0005cd19ea62150d71654d81
SewonYun <facemen9459@gmail.com>
2023-10-20 19:00:26 +0900

    [test] not amended file add commit from two prev commit

---
+++ app/src/test/java/com/happyint/menstrualcalendar/PeriodStartInsertTest.kt
@@ -0,0 +1,86 @@
+package com.happyint.menstrualcalendar
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import com.happyint.menstrualcalendar.entities.calendar.data.DayData
+import com.happyint.menstrualcalendar.entities.user.data.Information
+import com.happyint.menstrualcalendar.repositories.DayDataDao
+import com.happyint.menstrualcalendar.repositories.DayDataRepository
+import com.happyint.menstrualcalendar.repositories.InformationDao
+import com.happyint.menstrualcalendar.repositories.UserRepository
+import com.happyint.menstrualcalendar.viewModelFactories.CalendarViewModelFactory
+import com.happyint.menstrualcalendar.viewModelFactories.UserInfoViewModelFactory
+import com.happyint.menstrualcalendar.viewModels.CalendarViewModel
+import com.happyint.menstrualcalendar.viewModels.UserInfoViewModel
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.ParameterizedRobolectricTestRunner
+import org.robolectric.ParameterizedRobolectricTestRunner.Parameter
+import java.time.LocalDate
+
+@ExperimentalMaterial3Api
+@RunWith(ParameterizedRobolectricTestRunner::class)
+class PeriodStartInsertTest {
+
+    @Parameter
+    lateinit var localDate: LocalDate
+
+    companion object {
+        @JvmStatic
+        @ParameterizedRobolectricTestRunner.Parameters
+        fun data() = listOf(
+            LocalDate.parse("2023-01-01")
+        )
+    }
+
+    private lateinit var mockDatabase: AppDatabase
+    private lateinit var mockInformationDao: InformationDao
+    private lateinit var mockDayDataDao: DayDataDao
+    private lateinit var userInfoViewModel: UserInfoViewModel
+    private lateinit var calendarViewModel: CalendarViewModel
+
+    @Before
+    fun setup() {
+        mockDatabase = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            AppDatabase::class.java
+        ).allowMainThreadQueries().build()
+        mockInformationDao = mockDatabase.userDao()
+        mockDayDataDao = mockDatabase.dayDataDao()
+
+        userInfoViewModel =
+            UserInfoViewModelFactory(UserRepository(mockInformationDao)).create(UserInfoViewModel::class.java)
+        userInfoViewModel.updateUserInfo(Information(0, "test", "", 0))
+
+        calendarViewModel =
+            CalendarViewModelFactory(DayDataRepository(mockDayDataDao)).create(CalendarViewModel::class.java)
+
+    }
+
+    @Test
+    fun testInsertStartDate(): Unit = runBlocking {
+        // 테스트 데이터 설정
+        val selectedDayData = DayData(0, localDate, endDate = localDate, false)
+        val selectedDate: LocalDate = localDate
+
+        calendarViewModel.upsertDayData(selectedDayData).join()
+
+
+        val getDayData: DayData? = calendarViewModel.totalPeriodData.value.let { it ->
+            it.lastOrNull {
+                it.startDate == localDate
+            }
+        }
+
+        assert((selectedDate == getDayData?.startDate))
+    }
+
+    @After
+    fun tearDown() {
+        mockDatabase.close()
+    }
+}
