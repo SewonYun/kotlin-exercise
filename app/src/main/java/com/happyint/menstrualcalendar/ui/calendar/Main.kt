c86290b166b08d44d3cff1f5d1ef775bf7ec1ff5
SewonYun <facemen9459@gmail.com>
2023-10-24 21:18:54 +0900

    [todo] screen scroll twice jump error

--- app/src/main/java/com/happyint/menstrualcalendar/ui/calendar/Main.kt
+++ app/src/main/java/com/happyint/menstrualcalendar/ui/calendar/Main.kt
@@ -6,11 +6,18 @@ import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import com.happyint.menstrualcalendar.constants.UserPage
 import com.happyint.menstrualcalendar.customApi.testBorder
 import com.happyint.menstrualcalendar.ui.setting.TopBar
+import kotlinx.coroutines.flow.filter
 import java.time.YearMonth
 
 
@@ -23,17 +30,37 @@ fun LoadCalendar(currentScreen: MutableState<UserPage>) {
         TopBar(currentScreen = currentScreen)
 
         val currentMonth = YearMonth.now()
-        val months = List(5) { currentMonth.minusMonths(4).plusMonths(it.toLong()) }
-        val pagerState = rememberPagerState(initialPage = months.size / 2)
+        var months by remember {
+            mutableStateOf(List(5) {
+                currentMonth.minusMonths(2).plusMonths(it.toLong())
+            })
+        }
+        val pagerState = rememberPagerState(initialPage = 2)
+
+        // todo: 자꾸 이전 달 로 돌릴때 간헐적 으로 두번 튕김
+
+        LaunchedEffect(pagerState.currentPage) {
+            snapshotFlow { pagerState.isScrollInProgress }.filter { it == false }.collect {
+
+                if (pagerState.currentPage <= 1) {  // 첫 번째 페이지에 도달했을 때
+                    months = listOf(months.first().minusMonths(1)) + months  // 이전 달을 추가합니다.
+                    pagerState.scrollToPage(pagerState.currentPage + 1)
+                } else if (pagerState.currentPage >= months.size - 2) {  // 마지막 페이지에 도달했을 때
+                    months = months + listOf(months.last().plusMonths(1))  // 다음 달을 추가합니다.
+                }
+            }
+        }
 
         VerticalPager(
-            state = pagerState,
-            modifier = Modifier
-                .weight(1f)
-                .testBorder(),
-            pageCount = months.size,
-            beyondBoundsPageCount = 3
-        ) { page ->
+                state = pagerState,
+                modifier = Modifier
+                        .weight(1f)
+                        .testBorder(),
+                pageCount = months.size,
+                beyondBoundsPageCount = 3,
+
+                ) { page ->
+
             val month = months[page]
 
             OutSurface {
