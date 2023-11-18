a191aea38c33e7b1922dda602a424b27896183c2
SewonYun <facemen9459@gmail.com>
2023-11-18 12:58:25 +0900

    test: unclose event checking class

---
+++ app/src/test/java/com/happyint/cyclescape/service/calendar/UnclosedEventCheckerTest.kt
@@ -0,0 +1,60 @@
+package com.happyint.cyclescape.service.calendar
+
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import com.happyint.cyclescape.AppDatabase
+import com.happyint.cyclescape.entities.calendar.data.DayData
+import com.happyint.cyclescape.repositories.DayDataDao
+import com.happyint.cyclescape.repositories.DayDataRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import java.time.LocalDate
+
+@RunWith(RobolectricTestRunner::class)
+class UnclosedEventCheckerTest {
+
+    private lateinit var mockDatabase: AppDatabase
+    private lateinit var mockDayDataDao: DayDataDao
+    private lateinit var mockDataRepository: DayDataRepository
+
+    @Before
+    fun setup() {
+        mockDatabase = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            AppDatabase::class.java
+        ).allowMainThreadQueries().build()
+        mockDayDataDao = mockDatabase.dayDataDao()
+        mockDataRepository = DayDataRepository(mockDayDataDao)
+
+        CoroutineScope(Dispatchers.IO).launch {
+
+            mockDataRepository.upsert(
+                DayData(
+                    id = 0,
+                    startDate = LocalDate.now().minusDays(1),
+                    endDate = null,
+                    hasLittleNote = false
+                )
+            )
+
+        }
+
+    }
+
+    @Test
+    fun findUnClosedEvents(): Unit = runBlocking(Dispatchers.IO) {
+
+        val checker = UnclosedEventChecker(mockDataRepository)
+        val result = checker.findUnClosedEvents(LocalDate.now())
+
+        assertEquals(ProcessingResult.Success, result)
+
+    }
+}
