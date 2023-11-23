f70f1e5bc7e1f347b2f39c129354922c7950e4b9
SewonYun <facemen9459@gmail.com>
2023-11-24 00:32:04 +0900

    style(calendar): padding view top color change to outlineVariant

--- app/src/main/java/com/happyint/cyclescape/ui/calendar/Bottom.kt
+++ app/src/main/java/com/happyint/cyclescape/ui/calendar/Bottom.kt
@@ -1,20 +1,29 @@
 package com.happyint.cyclescape.ui.calendar
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun CalendarPaddingView() {
-    Box(
-        modifier = Modifier
-            .height(100.dp)
-            .fillMaxWidth()
-            .background(Color.Black)
-    )
+
+    Column {
+
+        Box(
+            modifier = Modifier
+                .height(100.dp)
+                .fillMaxWidth()
+                .border(width = 1.dp, color = MaterialTheme.colorScheme.outlineVariant)
+                .background(color = MaterialTheme.colorScheme.background)
+        )
+
+    }
+
 }
