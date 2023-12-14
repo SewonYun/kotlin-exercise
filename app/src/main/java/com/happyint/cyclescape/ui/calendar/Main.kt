5c38fa8524de85571b3615d23a4599dd48ece20b
SewonYun <facemen9459@gmail.com>
2023-12-15 08:38:32 +0900

    feat: VerticalPager speed optimization

--- app/src/main/java/com/happyint/cyclescape/ui/calendar/Main.kt
+++ app/src/main/java/com/happyint/cyclescape/ui/calendar/Main.kt
@@ -1,5 +1,7 @@
 package com.happyint.cyclescape.ui.calendar
 
+import android.annotation.SuppressLint
+import android.widget.Toast
 import androidx.compose.animation.core.FastOutLinearInEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.rememberSplineBasedDecay
@@ -12,55 +14,51 @@ import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.happyint.cyclescape.CycleScapeApplication
 import com.happyint.cyclescape.service.calendar.TopbarSyncDelegator
 import com.happyint.cyclescape.viewModels.CalendarViewModel
 import com.happyint.cyclescape.viewModels.ComposableCalendarViewModel
-import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
+@SuppressLint("CoroutineCreationDuringComposition")
 @ExperimentalFoundationApi
 @ExperimentalMaterial3Api
 @Composable
 fun LoadCalendar() {
 
     val composableCalendarViewModel = viewModel<ComposableCalendarViewModel>()
-    var months = composableCalendarViewModel.months.collectAsState()
+    val months = composableCalendarViewModel.months.collectAsState()
     val pagerState = composableCalendarViewModel.pagerState.collectAsState()
     val cv = viewModel<CalendarViewModel>()
     cv.updateUIStateByCopy(cv.uiState.collectAsState().value.copy(month = months.value[pagerState.value.currentPage]))
     TopbarSyncDelegator.UiStateUpdate(cv, months.value, pagerState.value)
 
-    LaunchedEffect(pagerState.value.currentPage) {
+    if (pagerState.value.currentPage <= 7) {  // 첫 번째 페이지에 도달했을 때
 
-        snapshotFlow { pagerState.value.isScrollInProgress }.filter { !it }.collect {
+        composableCalendarViewModel.updateMonth(
+            List(10) {
+                months.value.first().minusMonths(it.toLong() + 1)
+            }.reversed() + months.value
+        )
 
-            if (pagerState.value.currentPage <= 7) {  // 첫 번째 페이지에 도달했을 때
-
-                composableCalendarViewModel.updateMonth(
-                    List(10) {
-                        months.value.first().minusMonths(it.toLong() + 1)
-                    }.reversed() + months.value
-                )
-
-                pagerState.value.scrollToPage(pagerState.value.currentPage + 9)
-
-            } else if (pagerState.value.currentPage >= months.value.size - 7) {  // 마지막 페이지에 도달했을 때
-
-                composableCalendarViewModel.updateMonth(
-                    months.value + List(10) {
-                        months.value.last().plusMonths(it.toLong() + 1)
-                    }
-                )
-
-            }
-
+        CoroutineScope(Dispatchers.Main).launch {
+            pagerState.value.scrollToPage(pagerState.value.currentPage + 9)
         }
+
+    } else if (pagerState.value.currentPage >= months.value.size - 7) {  // 마지막 페이지에 도달했을 때
+
+        composableCalendarViewModel.updateMonth(
+            months.value + List(10) {
+                months.value.last().plusMonths(it.toLong() + 1)
+            }
+        )
 
     }
 
@@ -91,14 +89,23 @@ fun LoadCalendar() {
             flingBehavior = flingBehavior,
             beyondBoundsPageCount = 5,
             pageContent = {
-                val month = months.value[it]
+                if (it < months.value.size) {
+                    val month = months.value[it]
 
-                OutSurface {
+                    OutSurface {
 
-                    Column {
-                        CalendarBody(month)
+                        Column {
+                            CalendarBody(month)
+                        }
+
                     }
-
+                } else {
+                    Toast.makeText(
+                        CycleScapeApplication.instance,
+                        it.toString(),
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
                 }
             }
         )
